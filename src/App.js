ff531c91d05a8a392fc9065fd2f85f2c3551caea
multivoltage <diego.tonini93@gmail.com>
2017-11-16 02:11:12 +0200

    fucking style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import UserBar from './components/UserBar/UserBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import {cyan500} from 'material-ui/styles/colors';
+import './App.css'
+
+// components
+import HomeDescription from './components/HomeDescription'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <UserBar/>
+        <section id="main">
+          <HomeDescription/>
+        </section>
       </MuiThemeProvider>
     );
   }
---
+++ src/components/HomeDescription/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './homedescription.css'
+import {Card,CardText} from 'material-ui/Card';
+
+export default class HomeDescription extends Component {
+    
+  constructor(props){
+    super(props)
+  }
+  
+  render() {
+    return (
+      <div className="home-description">
+        <p className="title"><span className="title__app">Bookshare</span> ti permette di</p>
+
+        <span className="circle" />
+        <div className="contents">
+          <Card className="content">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+          <Card className="content">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+          <Card className="content">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
